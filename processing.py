ea082d43374be00a63ab64fe543aad3ac11899ef
Han <107281829+CamilleHan@users.noreply.github.com>
2024-05-30 17:53:55 -0400

    Update processing.py

--- processing.py
+++ processing.py
@@ -8,14 +8,9 @@ def process_weights(w, s_num):
     total_sample_num = sum(s_num)
     temp_sample_num = s_num[0]
     w_avg = copy.deepcopy(w[0])
-    # w1 = w2 = torch.tensor()
     g = np.zeros((len(w_avg.keys()),len(w)))
-    # gl = np.zeros((2))
-
-
 
     a = 0
-    # num = np.array(w.cpu())
     for k in w_avg.keys():  #the nn layer loop
         for i in range(1, len(w)):   #the client loop
             w_avg[k] += torch.mul(w[i][k], (s_num[i]/(temp_sample_num))).long()
@@ -26,12 +21,6 @@ def process_weights(w, s_num):
             # gl = np.linalg.norm(d)
             # gl[a] = np.linalg.norm(g[a, j])
         w_avg[k] = torch.mul(w_avg[k], temp_sample_num/(temp_sample_num))
-        # gl[a] = np.linalg.norm(g[a], axis=1)
-        # w1[k] = torch.norm(w[k], p=2)
-        # w2[k] = torch.norm(w[k], p=2)
-
         a =+ 1
     glob = np.linalg.norm(g)
-    # g_avg =  torch.norm(w_avg)
-
     return w_avg,glob
